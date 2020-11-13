fa97deda3a65886c0dda61a0f046e5f015a49025
DEV <dev@info.local>
2020-11-13 10:11:24 -0300

    salario

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace aumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float sal;
+            float nsal;
+            Console.WriteLine("Digite o seu salário atual: ");
+            sal = float.Parse(Console.ReadLine());
+            if (sal < 500)
+            {
+                nsal = (30 / 100) * sal;
+                Console.WriteLine("você receberá um aumento de 50%");
+                Console.WriteLine("Seu novo salário é: "+nsal+"$S");
+            }
+            else
+            {
+                Console.WriteLine("seu salário irá permanecer o mesmo.");
+            }
+        }
+    }
+}
